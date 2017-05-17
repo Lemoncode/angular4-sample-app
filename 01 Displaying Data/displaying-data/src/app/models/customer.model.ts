dd2694c5e61f03c6ec75bdd7d7b1aa6687ad89d7
Santiago Camargo <santi.camargo@lemoncode.net>
2017-05-17 09:30:43 +0200

    Removed commented method

--- 01 Displaying Data/displaying-data/src/app/models/customer.model.ts
+++ 01 Displaying Data/displaying-data/src/app/models/customer.model.ts
@@ -20,12 +20,3 @@ export class Customer {
         return Math.abs(ageDate.getUTCFullYear() - 1970);
     }
 }
-
-
-/*
-function _calculateAge(birthday) { // birthday is a date
-    var ageDifMs = Date.now() - birthday.getTime();
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-*/
