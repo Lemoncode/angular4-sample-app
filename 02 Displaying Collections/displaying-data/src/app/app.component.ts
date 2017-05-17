12bed72defc839c992ff4e276bbae1b5d19a0ff8
Santiago Camargo <santi.camargo@lemoncode.net>
2017-05-17 18:00:10 +0200

    Added changes for 02 Display Collection

--- 02 Displaying Collections/displaying-data/src/app/app.component.ts
+++ 02 Displaying Collections/displaying-data/src/app/app.component.ts
@@ -7,20 +7,20 @@ import { Customer } from './models/customer.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Displaying Data Demo';
-  private customers: Customer[];
-  customer: Customer;
+  title = 'Displaying Collections Demo';
+  customers: Customer[];
 
   constructor() {
-    this.customers = [
-      new Customer('Jai', 'Sal', '25 June 1981',
-      'http://vignette2.wikia.nocookie.net/disney/images/d/d6/ANGER_Fullbody_Render.png/revision/latest?cb=20150615084744'),
-      new Customer('Fer', 'Sal', '25 November 1984'),
-      new Customer('Lau', 'Sal', '04 March 2013')
-    ];
   }
 
   ngOnInit() {
-    this.customer = this.customers[0];
+    this.customers = [
+      new Customer('Jai', 'Sal', '25 June 1981',
+      'http://vignette2.wikia.nocookie.net/disney/images/d/d6/ANGER_Fullbody_Render.png/revision/latest?cb=20150615084744'),
+      new Customer('Fer', 'Sal', '25 November 1984',
+      'http://vignette4.wikia.nocookie.net/disney/images/8/82/SADNESS_Fullbody_Render.png/revision/latest?cb=20150615091236'),
+      new Customer('Lau', 'Sal', '04 March 2013',
+      'https://vignette2.wikia.nocookie.net/pixar/images/e/ed/Joy-Inside-Out-borders.jpg/revision/latest?cb=20150718214419')
+    ];
   }
 }
--- 02 Displaying Collections/displaying-data/src/app/app.module.ts
+++ 02 Displaying Collections/displaying-data/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CustomerSummaryComponent } from './customer/customer-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerSummaryComponent
   ],
   imports: [
     BrowserModule,
